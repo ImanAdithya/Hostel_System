4c9f461c0895a69e3de8468a69f65de04a04db5a
ImanAdithya <adithyaiman07@gmail.com>
2023-04-01 06:57:47 +0530

    complete packages

---
+++ src/lk/ijse/hostel/AppInitializer.java
@@ -0,0 +1,16 @@
+package lk.ijse.hostel;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch (args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/lk/ijse/hostel/controller/LoginFormController.java
@@ -0,0 +1,16 @@
+package lk.ijse.hostel.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public PasswordField txtPassword;
+    public TextField txtUserName;
+    public AnchorPane LoginPane;
+
+    public void btnSign(ActionEvent actionEvent) {
+
+    }
+}
